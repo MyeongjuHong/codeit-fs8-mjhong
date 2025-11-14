cc8904ba90e93cd6a08ebfb57137d0a9e436aea5
MyeongjuHong <hmjpearl@gmail.com>
2025-11-14 16:23:18 +0900

    feat: ts 파일 변경, js 확장자 제거, 미들웨어 type 적용, error 인터페이스 확장, Request 객체 타입 확장 처리,

---
+++ src/controllers/reviewController.ts
@@ -0,0 +1,87 @@
+import express, { NextFunction, Request, Response } from "express";
+import passport from "../config/passport.js";
+import reviewService from "../services/reviewService.js";
+import auth from "../middlewares/auth.js";
+import { Review } from "@prisma/client";
+const reviewController = express.Router();
+
+// TODO: 인증된 사용자만 리뷰 생성 가능하도록 수정
+reviewController.post(
+  "/",
+  auth.verifyAccessToken,
+  async (
+    req: Request<{}, {}, Omit<Review, "id" | "createdAt" | "updatedAt">>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { userId } = req.auth;
+    try {
+      const createdReview = await reviewService.create({
+        ...req.body,
+        authorId: userId,
+      });
+      return res.status(201).json(createdReview);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+reviewController.get(
+  "/:id",
+  async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const review = await reviewService.getById(id);
+      return res.json(review);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+reviewController.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const reviews = await reviewService.getAll();
+      return res.json(reviews);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+// TODO: 인증된 사용자만 리뷰 수정 가능하도록 수정
+// TODO: 리뷰 작성자만 수정 가능하도록 수정
+reviewController.put(
+  "/:id",
+  passport.authenticate("access-token", { session: false }),
+  auth.verifyReviewAuth,
+  async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    try {
+      const updatedReview = await reviewService.update(req.params.id, req.body);
+      return res.json(updatedReview);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+// TODO: 인증된 사용자만 리뷰 삭제 가능하도록 수정
+// TODO: 리뷰 작성자만 삭제 가능하도록 수정
+reviewController.delete(
+  "/:id",
+  auth.verifyAccessToken,
+  auth.verifyReviewAuth,
+  async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    try {
+      const deletedReview = await reviewService.deleteById(req.params.id);
+      return res.json(deletedReview);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+export default reviewController;
---
+++ src/controllers/userController.ts
@@ -0,0 +1,102 @@
+import express, { NextFunction, Request, Response } from "express";
+import userService from "../services/userService.js";
+import auth from "../middlewares/auth.js";
+import passport from "../config/passport.js";
+import { ValidationError } from "../types/errors.js";
+
+const userController = express.Router();
+
+userController.post(
+  "/users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, name, password } = req.body;
+      if (!email || !name || !password) {
+        const error = new ValidationError(
+          "email, name, password 가 모두 필요합니다."
+        );
+        throw error;
+      }
+      const user = await userService.createUser({ email, name, password });
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+userController.post(
+  "/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+    try {
+      if (!email || !password) {
+        const error = new ValidationError(
+          "email, password 가 모두 필요합니다."
+        );
+        throw error;
+      }
+      const user = await userService.getUser(email, password);
+
+      const accessToken = userService.createToken(user);
+      const refreshToken = userService.createToken(user, "refresh");
+      await userService.updateUser(user.id, { refreshToken });
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      });
+      return res.json({ ...user, accessToken });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+userController.post(
+  "/session-login",
+  auth.validateEmailAndPassword,
+  passport.authenticate("local"),
+  (req: Request, res: Response) => {
+    return res.json(req.user);
+  }
+);
+
+userController.post(
+  "/token/refresh",
+  passport.authenticate("refresh-token", { session: false }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    // QUES: Request 객체 타입 확장 처리가 무슨 뜻이지..?
+    try {
+      const refreshToken = req.cookies.refreshToken;
+      const { id } = req.user;
+
+      const { newAccessToken, newRefreshToken } =
+        await userService.refreshToken(id, refreshToken);
+      res.cookie("refreshToken", newRefreshToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        path: "/token/refresh",
+      });
+      return res.json({ accessToken: newAccessToken });
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+// TODO: 구글로그인 성공 시 토큰 발급 처리 하세요
+userController.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  (req: Request, res: Response, next: NextFunction) => {
+    // 구글로그인 성공 시 accessToken 과 refreshToken 을 발급합니다.
+    // 발급된 refreshToken 은 쿠키에 저장합니다.
+    // 발급된 accessToken 을 클라이언트에 응답합니다. 응답형식: { accessToken: 'accessToken' }
+  }
+);
+
+// TODO: GET /auth/google 엔드포인트를 만드세요
+
+export default userController;
